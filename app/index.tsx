2d77abc9aa8ea3553cd3d7e402ceef19b759be33
Diogo Rodrigo Pedreira Galvão <diogo.rgalvao@outlook.com>
2024-07-20 13:17:24 -0300

    Criação do projeto e instalação de dependências

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="bg-blue-900"
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
